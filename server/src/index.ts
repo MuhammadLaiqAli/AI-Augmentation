d99ea33a092eb20c6f0c097616122604ad993967
MuhammadLaiqAli <laiqazhar456@gmail.com>
2026-02-27 23:15:28 +0500

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.use("/chat", chatRouter);
-app.use("/images", imagesRouter);
+// app.use("/chat", chatRouter);
+// app.use("/images", imagesRouter);
 
 app.listen(3050, () => {
   console.log("Server started on port 3050");
